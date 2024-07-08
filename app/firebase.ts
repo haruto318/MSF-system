ab2037d59bedab49bbd740a07075140d01984a49
Haruto Hamano <haruto.yhs318@gmail.com>
2024-07-08 16:53:11 +0900

    complete system

--- app/firebase.ts
+++ app/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFb-ZZDKZDU5O3Jt0ELuRABvYESO1Yk5g",
-  authDomain: "summer-fes.firebaseapp.com",
-  projectId: "summer-fes",
-  storageBucket: "summer-fes.appspot.com",
-  messagingSenderId: "949189959835",
-  appId: "1:949189959835:web:793c5805363775b6e1a63e"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
